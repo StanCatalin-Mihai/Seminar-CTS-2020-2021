b01ad658f3313f770fbe985eb99edd62dbaabf1b
StanCatalin-Mihai <stancatalin18@stud.ase.ro>
2021-03-27 12:09:57 +0200

    Seminar 5 until break

---
+++ Seminar 5/src/ro/ase/cts/clase/Reteta.java
@@ -0,0 +1,53 @@
+package ro.ase.cts.clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reteta implements AbstractPrototype {
+    private List<String> denumiriSolutii;
+    private List<Integer> cantitati;
+
+    private Reteta() {
+    }
+
+    public Reteta(List<String> denumiriSolutii, List<Integer> cantitati) {
+        //Realizam validari pentru cantitatile solutiilor noastre.
+        this.denumiriSolutii = denumiriSolutii;
+        this.cantitati = cantitati;
+    }
+
+    public List<String> getDenumiriSolutii() {
+        return denumiriSolutii;
+    }
+
+    public void setDenumiriSolutii(List<String> denumiriSolutii) {
+        this.denumiriSolutii = denumiriSolutii;
+    }
+
+    public List<Integer> getCantitati() {
+        return cantitati;
+    }
+
+    public void setCantitati(List<Integer> cantitati) {
+        this.cantitati = cantitati;
+    }
+
+    @Override
+    public AbstractPrototype copiaza() {
+        Reteta copie = new Reteta();
+        copie.cantitati = new ArrayList<>();
+        copie.cantitati.addAll(this.cantitati);
+        copie.denumiriSolutii = new ArrayList<>();
+        copie.denumiriSolutii.addAll(this.denumiriSolutii);
+        return copie;
+    }
+
+//    @Override
+//    public String toString() {
+//        final StringBuilder sb = new StringBuilder("Reteta{");
+//        sb.append("denumiriSolutii=").append(denumiriSolutii);
+//        sb.append(", cantitati=").append(cantitati);
+//        sb.append('}');
+//        return sb.toString();
+//    }
+}
---
+++ Seminar 5/src/ro/ase/cts/program/Main.java
@@ -0,0 +1,21 @@
+package ro.ase.cts.program;
+
+import ro.ase.cts.clase.Reteta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> listaDenumiri = new ArrayList<>();
+        listaDenumiri.add("H2O");
+        listaDenumiri.add("CO2");
+        List<Integer> listaCantitati = new ArrayList<>();
+        listaCantitati.add(2);
+        listaCantitati.add(600);
+        Reteta reteta1 = new Reteta(listaDenumiri, listaCantitati);
+        System.out.println(reteta1.toString());
+        Reteta reteta2 = (Reteta)reteta1.copiaza();
+        System.out.println(reteta2.toString());
+    }
+}
